b8f0b7fb6bcb66a754778696b8cbc1952e9f5816
Jared Esquivel <jaredesquivel@MacBook-Air-de-Jared.local>
2025-11-09 22:18:14 -0600

    Initial commit

---
+++ PracticaAppConjuntoIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaAppConjuntoIOS
+//
+//  Created by Jared Esquivel on 09/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
